3eb9662427fdeee747bc734d53dcf9107e60c9ca
julian8112 <134758716+julian8112@users.noreply.github.com>
2023-06-24 00:18:31 -0500

    commit iniciar

---
+++ vista/html/inicio.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Sistema de Gestion Odontológica</title>
+        <link rel="stylesheet" type="text/css" href="Vista/css/estilos.css">
+    </head>
+    <body>
+        <div id="contenedor">
+            <div id="encabezado">
+                <h1>Sistema de Gestion Odontológica</h1>
+                </div>
+                <ul id="menu">
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="index.php?accion=asignar">Asignar</a></li>
+                    <li><a href="index.php?accion=consultar">Consultar Cita</a></li>
+                    <li><a href="index.php?accion=cancelar">Cancelar Cita</a></li>
+                </ul>
+                <div id="contenido">
+                    <h2>Informacion General</h2>
+                    <p>El Sistema de Gestion Odontológica permite administar la Informacion de los pacientes, tratamientos y citas a través de una interfaz web. </p>
+                    <p>El sistema cuenta con las siguientes secciones:
+                    <ul>
+                        <li>Asignar cita</li>
+                        <li>Consultar cita</li>
+                        <li>cancelar cita</li>
+                    </ul>
+                    </p>
+                </div>
+        </div>
+    </body>
+</html>
